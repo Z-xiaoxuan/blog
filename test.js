7d543f7d6bd3253fce37225af372848552a59722
ZJX <1115898584@qq.com>
2026-01-31 18:24:40 +0800

    feat: 包管理器

---
+++ test.js
@@ -0,0 +1,27 @@
+function fn(str) {
+  const stack = []
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === '}' && stack[stack.length - 1] === '{') {
+      stack.pop()
+    } else if (str[i] === ']' && stack[stack.length - 1] === '[') {
+      stack.pop()
+    } else if (str[i] === ')' && stack[stack.length - 1] === '(') {
+      stack.pop()
+    } else {
+      stack.push(str[i])
+    }
+  }
+
+  return stack.length === 0 ? true : false
+}
+
+console.log(fn('{(]}'))
+
+console.log(fn('{}[]{}'))
+
+console.log(fn('{]'))
+
+console.log(fn('{[)]'))
+
+console.log(fn('{[{}]}'))
